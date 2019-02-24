330853faccf98d6f374f171d8f784af238b36489
Valter <valter70@i.ua>
2019-02-24 18:49:46 +0200

    Добавлены константы ширины колонок на листах

--- src/Constants.kt
+++ src/Constants.kt
@@ -5,5 +5,7 @@ const val DEPART_CELL = 5
 const val GRAVITY_CELL = 6
 const val INPUT_XLS_FILE_NAME = "f2-12.xls"
 const val OUTPUT_XLS_FILE_NAME = "sluF2.xls"
+const val GLOBAL_COLUMN_WIDTH = 4800
+const val DETAIL_COLUMN_WIDTH = 4020
 val DEPART_COLUMN = 0..3
 val ARTICLE_COLUMN = 5..8

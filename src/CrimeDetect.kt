5f189b6759deee3d0cca1e8e4c9429588498218f
Valter <valter70@i.ua>
2019-03-10 13:03:20 +0200

    Добавлен вывод в консоль текстовых сообщений прогресса исполнения

--- src/CrimeDetect.kt
+++ src/CrimeDetect.kt
@@ -8,24 +8,29 @@ fun main(args: Array<String>) {
     val wbOut = HSSFWorkbook()
     val sheetGlobal = wbOut.createSheet("Загальна")
 
+    println("Формування статистики по службах...")
     writeHeaderOnMainSheet(wbOut)
     var endOfRange = FIRST_GLOBAL_INDICATOR_ROW + statByArticle.size + statByArticle185.size + statByGravity.size
     setRowHeightOnMainSheet(sheetGlobal, FIRST_GLOBAL_INDICATOR_ROW..endOfRange)
     writeDepartTabOnMainSheet(wbOut)
+
+    println("Формування статистики по статтях...")
     writeArticleTabOnMainSheet(wbOut)
 
+    println("Формування статистики служб по статтях...")
     val sheetDetail = wbOut.createSheet("Детальна")
     statByDepart.add(0, createTotalSumm(statByDepart))
     writeHeaderOnDetailSheet(wbOut)
     endOfRange = FIRST_DETAIL_INDICATOR_ROW + statByArticle.size + statByArticle185.size
     setRowHeightOnMainSheet(sheetDetail, FIRST_DETAIL_INDICATOR_ROW..endOfRange)
-
     writeTabOnDetailSheet(wbOut)
 
+    println("Визначення параметрів документа для друку...")
     setPrintSetup(wbOut)
 
     wbOut.setActiveSheet(1)
 
     closeOutXLSFile(wbOut)
 
+    println("End of hard work!")
 }
